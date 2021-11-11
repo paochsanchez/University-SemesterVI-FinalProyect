37da121cc9fea98f578286dcd237722b85daf5ff
paochsanchez <pchinchilla@estudiantec.com>
2021-11-10 20:32:22 -0600

    Read CSV file

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #Aquí iría nuestro proyecto
 #SI HUBIERA UNO :(
+    
+import csv
+
+def readCSVFile():
+    with open('test-set.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter='\t')
+        print(csv_reader.fieldnames) 
+        for row in csv_reader:
+            print(row)
+
+readCSVFile()
